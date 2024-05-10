d26711022d6a8fd13c87a57eb777801271fa845f
Piyush Kumar Singh <96623633+P123yu@users.noreply.github.com>
2024-05-10 12:23:12 +0530

    Update UserModel.java

--- src/main/java/com/onetooneuni/model/UserModel.java
+++ src/main/java/com/onetooneuni/model/UserModel.java
@@ -1,21 +1,15 @@
 package com.onetooneuni.model;
-
-
 import jakarta.persistence.*;
 import lombok.*;
-
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserModel {
-	
+public class UserModel {	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long userId;
-	
+	private Long userId;	
 	private Long rollNo;
 	private String userName;
 	private Double userMarks;
